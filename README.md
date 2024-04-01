262cc79905e88c1f0867e3954c80557b1101ef81
Terry Tan <fuling19.me@gmail.com>
2024-04-01 10:25:33 +0800

    import apm

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Frontend of [spring-practice-after-course](https://github.com/a-fly-fly-bird/spring-practice-after-course).
+
+APM: [Docker-Compose-ElasticAPM-Stack](https://github.com/a-fly-fly-bird/Docker-Compose-ElasticAPM-Stack)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApmModule, ApmService } from '@elastic/apm-rum-angular';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
@@ -8,10 +9,23 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, ApmModule],
+  providers: [ApmService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   isCollapsed = false;
+  constructor(apmService: ApmService) {
+    // Agent API is exposed through this apm instance
+    const apm = apmService.init({
+      serviceName: 'angular-app',
+      serverUrl: 'http://localhost:8200'
+    })
+
+    apm.setUserContext({
+      'username': 'foo',
+      'id': 'bar'
+    })
+  }
 }

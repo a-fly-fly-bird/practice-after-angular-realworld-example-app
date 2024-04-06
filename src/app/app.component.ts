40df4987b4b7ab4aa7be6922cb4c9de5c6d97662
Terry Tan <fuling19.me@gmail.com>
2024-04-06 23:33:03 +0800

    实现footer始终固定在底部

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApmModule, ApmService } from '@elastic/apm-rum-angular';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { HeaderComponent } from './core/layout/header/header.component';
 import { DemoNgZorroAntdModule } from './ng-zorro-antd.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ApmModule, DemoNgZorroAntdModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ApmModule,
+    DemoNgZorroAntdModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
   providers: [ApmService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
